6feec77f40ab5c92b46659fa367d700df67d00df
Tejas <tejasupadhyay48@gmail.com>
2025-06-29 17:00:20 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import './App.css'
 import Pokedex from './components/Pokedex/Pokedex';
 import Search from './components/search/Search.jsx';
 // import Search from './components/search/Search';
+import { Link } from 'react-router-dom';
+import CustomRoutes from './routes/CustomRoutes';
 
 
 
 function App() {
   
   return (
-    <div>
-      <Pokedex/>
+    <div className='outer-pokedex'>
+
+       <h1 id="Pokedex-heading">
+        <Link to="/">Pokedex</Link>
+        </h1>
+      <CustomRoutes/>
       
     </div>
   )
